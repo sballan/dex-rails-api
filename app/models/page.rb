5285ba172f8fd76d784caae524a2e4ec93aaa484
Samuel Ballan <sgb4622@gmail.com>
2020-11-17 18:11:42 -0500

    pages linked to and from

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,5 +3,8 @@ class Page < ApplicationRecord
   has_many :links_to, inverse_of: :from, foreign_key: :from_id, class_name: "Link"
   has_many :links_from, inverse_of: :to, foreign_key: :to_id, class_name: "Link"
 
+  has_many :pages_linked_to, through: :links_to, source: :to
+  has_many :pages_linked_from, through: :links_from, source: :from
+
   validates_presence_of :url
 end
