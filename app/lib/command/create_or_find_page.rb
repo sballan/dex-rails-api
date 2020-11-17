ed8655ba0c9b20a55a4fcdc1948f0c2b9b3d6aab
Samuel Ballan <sgb4622@gmail.com>
2020-11-17 12:49:51 -0500

    nice little create or find page command

---
+++ app/lib/command/create_or_find_page.rb
@@ -0,0 +1,16 @@
+module Command
+  class CreateOrFindPage < Command::Base::Abstract
+    def initialize(url)
+      @url = url
+
+      @result = Command::Base::Result.new
+    end
+
+    def run
+      page = Page.create_or_find_by!(url: @url)
+      result.succeed!(page)
+    rescue StandardError => e
+      result.fail!(e)
+    end
+  end
+end
