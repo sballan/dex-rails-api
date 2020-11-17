d2fb223eb10cc7d6ca4e3e31fa22ef8d3a6b0ca4
Samuel Ballan <sgb4622@gmail.com>
2020-11-17 11:58:00 -0500

    nest those commands!

--- app/lib/command/update_page.rb
+++ app/lib/command/update_page.rb
@@ -11,12 +11,20 @@ module Command
       download_url_command = Command::DownloadUrl.new(@page.url)
       download_url_command.run
 
-      if download_url_command.success?
-        upload_page_to_s3_command = Command::UploadPageToS3.new(@page.id, download_url_command.payload)
-        result.succeed!(nil, [upload_page_to_s3_command])
-      else
+      if download_url_command.failure?
         result.fail!(download_url_command.error, [download_url_command])
+        return
       end
+
+      upload_page_to_s3_command = Command::UploadPageToS3.new(@page.id, download_url_command.payload)
+      upload_page_to_s3_command.run
+
+      if upload_page_to_s3_command.failure?
+        result.fail!(upload_page_to_s3_command.error, [download_url_command, upload_page_to_s3_command])
+        return
+      end
+
+      result.succeed!(nil, [download_url_command, upload_page_to_s3_command])
     end
   end
 end
