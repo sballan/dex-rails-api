990ed699b363555d25004b2d969fc04bf7903ff7
Samuel Ballan <sgb4622@gmail.com>
2020-11-20 09:50:06 -0500

    should allow for page title

--- app/lib/command/create_or_find_page.rb
+++ app/lib/command/create_or_find_page.rb
@@ -1,13 +1,20 @@
 module Command
   class CreateOrFindPage < Command::Base::Abstract
-    def initialize(url)
+    def initialize(url, title = nil)
       @url = url
+      @title = title
 
       @result = Command::Base::Result.new(self.class.name)
     end
 
     def run
       page = Page.create_or_find_by!(url: @url)
+
+      if page.title != @title
+        page.title = @title
+        page.save!
+      end
+
       result.succeed!(page)
     rescue StandardError => e
       result.fail!(e)
