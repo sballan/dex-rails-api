b38219245417f87c79e94a49ff2a18ea7bd056f1
Samuel Ballan <sgb4622@gmail.com>
2020-11-22 13:34:13 -0500

    sidekiq configuration

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,11 @@
+Sidekiq.configure_server do |config|
+  config.redis = {
+      url: ENV['SIDEKIQ_REDIS_URL'] || ENV['REDIS_URL']
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+      url: ENV['SIDEKIQ_REDIS_URL'] || ENV['REDIS_URL']
+  }
+end
