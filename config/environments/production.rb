bf72e802fd1fbd5e0273ac5dec4cee26cd042764
Samuel Ballan <sgb4622@gmail.com>
2020-11-22 13:52:17 -0500

    fix sidekiq?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   config.active_job.queue_adapter     = :sidekiq
-  config.active_job.queue_name_prefix = "dex_rails_api_production"
+  # config.active_job.queue_name_prefix = "dex_rails_api_production" # NOTE: do I want a queue name prefix?
 
   config.action_mailer.perform_caching = false
 
