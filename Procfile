1dc4965c19ec4aab8ae21e0282c1a7dd2eef8e35
Samuel Ballan <sgb4622@gmail.com>
2020-11-22 13:24:52 -0500

    add procfile and sidekiq routes

---
+++ Procfile
@@ -0,0 +1,2 @@
+web:  bin/rails server -p ${PORT:-5000} -e $RAILS_ENV
+worker: sidekiq --environment $RAILS_ENV
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+  mount Sidekiq::Web => '/sidekiq'
+
   get 'info', to: 'info#index'
   root 'info#index'
 end
