39ab900ac7634392c1c24208a3b062ed0a77cd4e
Samuel Ballan <sgb4622@gmail.com>
2020-11-22 19:17:01 -0500

    direct handling of mechanize page

---
+++ app/lib/command/download_mechanize_page.rb
@@ -0,0 +1,30 @@
+module Command
+  class DownloadMechanizePage < Command::Base::Abstract
+    def initialize(url)
+      @url = url
+      @mechanize_page = nil
+
+      @result = Command::Base::Result.new(self.class.name)
+    end
+
+    def run
+      raise 'Page is nil' if mechanize_page.nil?
+      raise 'Only html pages are supported' unless mechanize_page.is_a?(Mechanize::Page)
+
+      result.succeed!(mechanize_page)
+    rescue Mechanize::RobotsDisallowedError, Mechanize::ResponseCodeError => e
+      result.fail!(e)
+    end
+
+    private
+
+    def mechanize_page
+      return @mechanize_page unless @mechanize_page.nil?
+
+      mechanize_agent = Mechanize.new
+      mechanize_agent.history.max_size = 1 # default is 50
+      mechanize_agent.robots = true
+      @mechanize_page = mechanize_agent.get(@url)
+    end
+  end
+end
---
+++ app/lib/command/process_mechanize_page.rb
@@ -0,0 +1,54 @@
+module Command
+  class ProcessMechanizePage < Command::Base::Abstract
+    # @param [Page] page
+    # @param [Mechanize::Page] mechanize_page
+    def initialize(page, mechanize_page)
+      @page = page
+      @mechanize_page = mechanize_page
+
+      @result = Command::Base::Result.new(self.class.name)
+    end
+
+    def run
+      title = @mechanize_page.title
+      page.title = title
+      page.save!
+
+      extract_page_links
+
+      result.succeed!(page)
+    end
+
+    private
+
+
+    def extract_page_links
+      links_to = @mechanize_page.links&.map do |mechanize_link|
+        create_page_link_result = create_page_link(mechanize_link)
+        create_page_link_result.payload
+        # rescue StandardError
+        #   nil
+      end
+
+      if failed_result = result.results.find(&:failure?)
+        Rails.logger.warn "Found a bad link!"
+        result.fail!(failed_result.error)
+        raise failed_result.error
+      else
+        @page.links_to = links_to
+        @page.save!
+      end
+    end
+
+    # @param [Mechanize::Page::Link] mechanize_link
+    def create_page_link(mechanize_link)
+      url = mechanize_link.resolved_uri.to_s
+      text = mechanize_link.text.strip.gsub(/\s/, " ")
+      text = nil if text.empty?
+
+      create_page_link_command = Command::CreatePageLink.new(@page, url, text)
+      run_nested(create_page_link_command)
+      create_page_link_command.result
+    end
+  end
+end
