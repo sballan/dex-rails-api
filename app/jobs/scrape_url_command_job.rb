8940e6b816fc5269ed233677d7b680ba05bca553
Samuel Ballan <sgb4622@gmail.com>
2020-11-24 13:25:13 -0500

    who knows...

--- app/jobs/scrape_url_command_job.rb
+++ app/jobs/scrape_url_command_job.rb
@@ -2,8 +2,11 @@ class ScrapeUrlCommandJob < ApplicationJob
   queue_as :default
 
   def perform(url, force=false)
+    GC.start(full_mark: true, immediate_sweep: true)
+
     command = Command::ScrapeUrl.new(url)
     force ? command.run! : command.run
+
     GC.start(full_mark: true, immediate_sweep: true)
   end
 end
