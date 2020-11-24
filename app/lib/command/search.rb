e4d70cd8a9ce8804da594eaf1dfe9b36ea323639
Samuel Ballan <sgb4622@gmail.com>
2020-11-24 12:03:34 -0500

    search sauce

--- app/lib/command/search.rb
+++ app/lib/command/search.rb
@@ -2,14 +2,18 @@ module Command
   class Search < Command::Base::Abstract
     def initialize(text)
       super()
-      @text = text
+      @text = text.downcase
     end
 
     def run_proc
-      full_text = @text
-      text_words = @text.split(/\s/).map {|w| "%#{w}%"}
-      text_words << full_text
-      Query.includes(:result).where(Query.arel_table[:text].matches_any(text_words))
+      matches_array = []
+      text_words = @text.split(/\s/)
+
+      matches_array << @text
+      matches_array.concat(text_words)
+      matches_array.concat(text_words.map {|w| "%#{w}%"})
+
+      result.succeed!(Page.for_query_text(matches_array).by_links_from_count)
     end
   end
 end
