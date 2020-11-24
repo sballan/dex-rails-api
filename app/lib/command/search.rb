e385a33a0e37ed760df7bbb823fb8383fae32f9f
Samuel Ballan <sgb4622@gmail.com>
2020-11-24 17:30:15 -0500

    search needs some limits

--- app/lib/command/search.rb
+++ app/lib/command/search.rb
@@ -1,8 +1,9 @@
 module Command
   class Search < Command::Base::Abstract
-    def initialize(text)
+    def initialize(text, results_count=20)
       super()
       @text = text.downcase
+      @results_count = results_count
     end
 
     def run_proc
@@ -13,7 +14,8 @@ module Command
       matches_set.merge(text_words)
       matches_set.merge(text_words.map {|w| "%#{ActiveRecord::Base.send(:sanitize_sql_like, w)}%"})
 
-      result.succeed!(Page.for_query_text(matches_set.to_a).by_links_from_count)
+      pages = Page.for_query_text(matches_set.to_a).by_links_from_count.limit(@results_count)
+      result.succeed!(pages)
     end
   end
 end
