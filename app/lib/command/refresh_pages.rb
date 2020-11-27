36fd6cba9f25f3de53f1cf3cc2d6b82c7c20a9d9
Samuel Ballan <sgb4622@gmail.com>
2020-11-27 15:49:05 -0500

    refresh multiple pages with silly rate limit avoiding algorithm

---
+++ app/lib/command/refresh_pages.rb
@@ -0,0 +1,25 @@
+module Command
+  class RefreshPages < Command::Base::Abstract
+    def initialize(urls)
+      super()
+      @urls = urls
+    end
+
+    def run_proc
+      urls_by_host = @urls.group_by {|url| URI.parse(url).host }
+      # while any host still has unprocessed urls
+      while(urls_by_host.any? {|host, urls| urls.any?}) do
+        # pop a url for each host, refresh them all (since we don't worry about rate limits), then sleep
+        urls_by_host.each do |host, urls|
+          next unless urls.any?
+          url = urls.pop
+          command = Command::RefreshPage.new url
+          run_nested!(command)
+        end
+        sleep 3
+      end
+
+      result.succeed!
+    end
+  end
+end
