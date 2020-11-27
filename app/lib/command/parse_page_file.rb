0345b5e35022630589427dbaa82e489d167f8335
Samuel Ballan <sgb4622@gmail.com>
2020-11-27 15:23:57 -0500

    parsing page.  annoying that we need the url to parse relative hrefs...

---
+++ app/lib/command/parse_page_file.rb
@@ -0,0 +1,35 @@
+module Command
+  class ParsePageFile < Base::Abstract
+    def initialize(url, page_file)
+      super()
+      @url = url
+      @page_file = page_file
+    end
+
+    def run_proc
+      parsed_page = parse_page
+      result.succeed!(parsed_page)
+    end
+
+    private
+
+    def parse_page
+      doc = Nokogiri::HTML(@page_file)
+
+      parsed_page = {
+          title: doc.title,
+          body: Html2Text.convert(doc.to_html.force_encoding('UTF-8')),
+          links: []
+      }
+
+      doc.css('a').each do |link_node|
+        parsed_page[:links] << {
+            url: URI.parse(@url).merge(URI.parse(link_node['href'])).to_s,
+            text: link_node.content.blank? ? nil : link_node.content
+        }
+      end
+
+      parsed_page
+    end
+  end
+end
