3bac468977425d041c57ba39799ae47d4d290ebc
Samuel Ballan <sgb4622@gmail.com>
2020-11-27 23:35:47 -0500

    insert links, very complicated class!

---
+++ app/lib/command/insert_links.rb
@@ -0,0 +1,54 @@
+module Command
+  class InsertLinks < Command::Base::Abstract
+    def initialize(page, links_array)
+      super()
+      @page = page
+      @links_by_url = {}
+      links_array.each do |link|
+        @links_by_url[link[:url]] = link
+      end
+
+      @links_by_url.each do |url, link|
+        link.delete(:url)
+        link[:from_id] = @page.id
+        link[:created_at] = DateTime.now.utc
+        link[:updated_at] = DateTime.now.utc
+      end
+    end
+
+    def run_proc
+      # This is strange...but we're managing stupid timestamps here. For some reason I can't seem
+      # to get default values for timestamps.  For now - we need to include created at and updated at.
+      # Since we're doing unique by url, this will only create new pages (not affect existing ones), so this is safe.
+      #
+      # Rails does have "returning", but only works for postgres.  That's why we fetch_to_ids after this statement
+      Page.insert_all(page_insert_attributes, unique_by: :url)
+      fetch_to_ids
+      Link.insert_all(@links_by_url.values)
+
+      result.succeed!
+    end
+
+    private
+
+    def fetch_to_ids
+      # Once pages have been inserted, we can get all the ids and urls we need in a single query
+      Page.where(url: @links_by_url.keys).in_batches do|pages|
+        binding.pry
+        pages.pluck(:id, :url).each do |page|
+          @links_by_url[page.second][:to_id] = page.first
+        end
+      end
+    end
+
+    def page_insert_attributes
+      @links_by_url.map do |url, link|
+        {
+          url: url,
+          created_at: link[:created_at],
+          updated_at: link[:updated_at]
+        }
+      end
+    end
+  end
+end
