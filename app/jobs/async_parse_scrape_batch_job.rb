4622e2806dc3836d30d0c3761084b7ecb1bae689
Samuel Ballan <sgb4622@gmail.com>
2020-12-01 12:59:33 -0500

    pass the ball back and forth

--- app/jobs/async_parse_scrape_batch_job.rb
+++ app/jobs/async_parse_scrape_batch_job.rb
@@ -1,11 +1,25 @@
 class AsyncParseScrapeBatchJob < ApplicationJob
   queue_as :parse
 
-  def perform(scrape_batch_id)
+  def perform(scrape_batch_id, ttl=1.minute)
+    start_time = Time.now.to_i
+    end_time = start_time + ttl.to_i
+
     scrape_batch = ScrapeBatch.find(scrape_batch_id)
-    scrape_batch.parse_active! if scrape_batch.parse_ready?
+
+    if scrape_batch.parse_ready?
+      scrape_batch.parse_active!
+    elsif !scrape_batch.parse_active? && scrape_batch.scrape_pages.refresh_success.parse_ready.count > 0
+      scrape_batch.parse_finished_at = nil
+      scrape_batch.parse_active?
+    end
+
+    scrape_batch.save!
+    scrape_batch.reload
 
     run_scrape_batch_command = Parse::ParseScrapeBatch.new(scrape_batch)
     run_scrape_batch_command.run_with_gc!
+
+    AsyncRefreshScrapeBatchJob.perform_later(scrape_batch_id, end_time - Time.now.to_i)
   end
 end
--- app/jobs/async_refresh_scrape_batch_job.rb
+++ app/jobs/async_refresh_scrape_batch_job.rb
@@ -1,31 +1,27 @@
 class AsyncRefreshScrapeBatchJob < ApplicationJob
   queue_as :refresh
 
-  def perform(scrape_batch_id, ttl=10.minutes)
+  def perform(scrape_batch_id, ttl=1.minute)
     start_time = Time.now.to_i
     end_time = start_time + ttl.to_i
 
     scrape_batch = ScrapeBatch.find scrape_batch_id
-    scrape_batch.refresh_active! if scrape_batch.refresh_ready?
 
-    first_run = true
 
-    while scrape_batch.refresh_active? && (Time.now.to_i < end_time) do
+    if scrape_batch.refresh_ready?
+      scrape_batch.refresh_active!
+    elsif !scrape_batch.refresh_active? && scrape_batch.scrape_pages.refresh_ready.count > 0
+      scrape_batch.refresh_finished_at = nil
+      scrape_batch.refresh_active!
+    end
+
+    scrape_batch.save!
+    scrape_batch.reload
+
+    if scrape_batch.refresh_active? && (Time.now.to_i < end_time)
       run_scrape_batch_command = Refresh::RefreshScrapeBatch.new(scrape_batch)
       run_scrape_batch_command.run_with_gc!
-      AsyncParseScrapeBatchJob.perform_later(scrape_batch.id)
-
-      # silly hack, but let's give the parsing a chance to catch up before do these checks
-      sleep 30.seconds
-
-      scrape_batch.reload
-      # Gross - but the first time this runs, there won't be any parsing happening...
-      if !scrape_batch.refresh_active? && scrape_batch.scrape_pages.refresh_ready.count > 0
-        scrape_batch.refresh_finished_at = nil
-        scrape_batch.refresh_active!
-        scrape_batch.save!
-        scrape_batch.reload
-      end
+      AsyncParseScrapeBatchJob.perform_later(scrape_batch.id, end_time - Time.now.to_i)
     end
   end
 end
