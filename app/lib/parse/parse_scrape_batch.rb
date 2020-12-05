67b993a676914ebea8bab058efc6aac17f0e64d7
Samuel Ballan <sgb4622@gmail.com>
2020-12-05 14:44:32 -0500

    gather pages belongs in parse

--- app/lib/parse/parse_scrape_batch.rb
+++ app/lib/parse/parse_scrape_batch.rb
@@ -7,17 +7,12 @@ module Parse
      end
 
     def run_proc
-      startup_checks
-      handle_start!
-
       parse_scrape_pages
-      @scrape_batch.reload
+      gather_new_pages
 
-      handle_success!
       result.succeed!(@scrape_batch)
     rescue StandardError => e
       Rails.logger.error "ScrapeBatch (#{@scrape_batch.id}) failed"
-      handle_failure
       raise e
     end
 
@@ -27,54 +22,23 @@ module Parse
       # NOTE: need to make sure we only get ones with refresh success. "parse ready" is a misnomer
       @scrape_batch.scrape_pages.refresh_success.parse_ready.in_batches.each_record do |scrape_page|
         command = Parse::ParseScrapePage.new scrape_page
-        run_nested_with_gc(command)
+        run_nested_with_gc!(command)
       end
     end
 
-    def startup_checks
-      if @scrape_batch.active?
-        Rails.logger.debug "Starting already active ScrapeBatch (#{@scrape_batch.id})"
-      elsif @scrape_batch.ready?
-        Rails.logger.debug "Starting ready ScrapeBatch (#{@scrape_batch.id})"
-      else
-        raise "Something went wrong"
+    def gather_new_pages
+      Rails.logger.debug "Gathering newly created pages"
+      links = []
+      @scrape_batch.links.in_batches do |links|
+        scrape_page_attributes = links.all.map do |link|
+          {
+              scrape_batch_id: @scrape_batch.id,
+              page_id: link.to_id
+          }
+        end
+        Rails.logger.debug "Adding newly created pages to batch"
+        ScrapePage.insert_all(scrape_page_attributes, unique_by: :index_scrape_pages_on_scrape_batch_id_and_page_id)
       end
-    end
-
-    def handle_start!
-      @scrape_batch.active!
-      @scrape_batch.started_at ||= DateTime.now.utc
-      @scrape_batch.parse_started_at ||= DateTime.now.utc
-      @scrape_batch.save!
-    end
-
-    def handle_failure
-      @scrape_batch.parse_failure!
-      @scrape_batch.parse_failure_at = DateTime.now.utc
-      @scrape_batch.save
-    end
-
-    # Need to work out the logic here for determining when to stop.  I think
-    # we may need to this "upstream", so that each worker determines the status of the *other* workers, not themselves.
-    def handle_success!
-      # If there are still pages to parse at this point, raise an error...that shouldn't happen
-      if @scrape_batch.scrape_pages.refresh_success.parse_ready.count != 0
-        raise "This shouldn't be possible, we just succeeded..."
-      else
-        @scrape_batch.parse_finished_at = DateTime.now.utc
-      end
-
-      # If after parsing there are not pages to be refreshed, we've finished refreshing
-      if @scrape_batch.scrape_pages.refresh_ready.count == 0
-        Rails.logger.debug "ScrapeBatch (#{@scrape_batch.id}) has no pages left to refresh after parsing."
-        @scrape_batch.refresh_finished_at ||= DateTime.now.utc
-      else
-        Rails.logger.debug "ScrapeBatch (#{@scrape_batch.id}) does have pages left to refresh after parsing."
-        @scrape_batch.refresh_active!
-        @scrape_batch.refresh_finished_at = nil
-      end
-
-      @scrape_batch.save!
     end
   end
 end
--- app/lib/refresh/refresh_scrape_batch.rb
+++ app/lib/refresh/refresh_scrape_batch.rb
@@ -7,24 +7,20 @@ module Refresh
      end
 
     def run_proc
-      startup_checks
-      handle_start!
-
+      Rails.logger.debug "[Refresh::RereshScrapeBatch] ScrapeBatch (#@scrape_batch) starting"
       refresh_scrape_pages
-      gather_new_pages
-      @scrape_batch.reload
 
-      handle_success!
+      Rails.logger.debug "[Refresh::RereshScrapeBatch] ScrapeBatch (#@scrape_batch) finished"
       result.succeed!(@scrape_batch)
     rescue StandardError => e
       Rails.logger.error "ScrapeBatch (#{@scrape_batch.id}) failed"
-      handle_failure
       raise e
     end
 
     private
 
     def refresh_scrape_pages
+      # group by host for some janky 'rate limiting'
       scrape_pages_by_host = @scrape_batch.scrape_pages.refresh_ready.group_by do |scrape_page|
         URI.parse(scrape_page.page.url).host
       end
@@ -36,68 +32,11 @@ module Refresh
           next unless scrape_pages.any?
           scrape_page = scrape_pages.pop
           command = Refresh::RefreshScrapePage.new scrape_page
-          run_nested_with_gc(command)
+          run_nested_with_gc!(command)
         end
         sleep 2
       end
     end
 
-    def gather_new_pages
-      Rails.logger.debug "Gathering newly created pages"
-      links = []
-      @scrape_batch.links.includes(:to).in_batches do |links|
-        scrape_page_attributes = links.all.map do |link|
-          {
-            scrape_batch_id: @scrape_batch.id,
-            page_id: link.to_id
-          }
-        end
-        Rails.logger.debug "Adding newly created pages to batch"
-        ScrapePage.insert_all(scrape_page_attributes, unique_by: :index_scrape_pages_on_scrape_batch_id_and_page_id)
-      end
-    end
-
-    def startup_checks
-      if @scrape_batch.active?
-        Rails.logger.debug "Starting already active ScrapeBatch (#{@scrape_batch.id})"
-      elsif @scrape_batch.ready?
-        Rails.logger.debug "Starting ready ScrapeBatch (#{@scrape_batch.id})"
-      else
-        raise "Something went wrong"
-      end
-    end
-
-    def handle_start!
-      @scrape_batch.active!
-      @scrape_batch.started_at ||= DateTime.now.utc
-      @scrape_batch.refresh_started_at ||= DateTime.now.utc
-      @scrape_batch.save!
-    end
-
-    def handle_failure
-      @scrape_batch.refresh_failure!
-      @scrape_batch.refresh_failure_at = DateTime.now.utc
-      @scrape_batch.save
-    end
-
-    def handle_success!
-      # If there are none left to refresh, we've finished
-      if @scrape_batch.scrape_pages.refresh_ready.count > 0
-        Rails.logger.debug "ScrapeBatch (#{@scrape_batch.id}) still has pages left to refresh. Status should remain refresh_active"
-      else
-        Rails.logger.debug "ScrapeBatch (#{@scrape_batch.id}) has no pages left to refresh. Setting refresh_finished_at"
-        @scrape_batch.refresh_finished_at = DateTime.now.utc
-
-        # If none failed, we've succeeded
-        if @scrape_batch.scrape_pages.refresh_failure.count == 0
-          Rails.logger.debug "ScrapeBatch (#{@scrape_batch.id}) has no pages left to refresh. Setting refresh_finished_at"
-          @scrape_batch.refresh_success!
-        else
-          # TODO: some retry mechanism.  use 'dead' status to signify not retrying
-          @scrape_batch.refresh_failure!
-        end
-        @scrape_batch.save!
-      end
-    end
   end
 end
