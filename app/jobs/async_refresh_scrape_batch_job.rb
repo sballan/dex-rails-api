524c11c2428286c64a1ce1bb3010ca47756b711f
Samuel Ballan <sgb4622@gmail.com>
2020-12-05 14:46:33 -0500

    logging

--- app/jobs/async_refresh_scrape_batch_job.rb
+++ app/jobs/async_refresh_scrape_batch_job.rb
@@ -14,17 +14,17 @@ class AsyncRefreshScrapeBatchJob < ApplicationJob
     if scrape_batch.scrape_pages.refresh_ready.any? && (Time.now.to_i < end_time)
       num_to_refresh = scrape_batch.scrape_pages.refresh_ready.count
 
-      Rails.logger.debug "More pages to refresh! Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
+      Rails.logger.debug "[AsyncRefreshScrapeBatchJob] More pages to refresh! Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
       run_scrape_batch_command = Refresh::RefreshScrapeBatch.new(scrape_batch)
       run_scrape_batch_command.run_with_gc!
 
       num_left = scrape_batch.scrape_pages.refresh_ready.count
 
-      Rails.logger.debug "We went from #{num_to_refresh} to #{num_left}.  About to hand over for parsing."
+      Rails.logger.debug "[AsyncRefreshScrapeBatchJob] We went from #{num_to_refresh} to #{num_left}.  About to hand over for parsing."
 
       AsyncParseScrapeBatchJob.perform_later(scrape_batch.id, end_time - Time.now.to_i)
     else
-      Rails.logger.debug "No pages left to refresh. Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
+      Rails.logger.info "[AsyncRefreshScrapeBatchJob] No pages left to refresh. Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
     end
   end
 
