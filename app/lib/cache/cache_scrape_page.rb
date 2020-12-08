7013fa77e4343efe7c9bbc2dee7dfaca95496e5d
Samuel Ballan <sgb4622@gmail.com>
2020-12-08 13:08:17 -0500

    don't try to cache if text is empty!

--- app/lib/cache/cache_scrape_page.rb
+++ app/lib/cache/cache_scrape_page.rb
@@ -8,8 +8,7 @@ module Cache
     def run_proc
       cache_title
       cache_links
-
-      create_body_queries
+      cache_body
 
       result.succeed!
     end
@@ -17,6 +16,10 @@ module Cache
     private
 
     def cache_title
+      if @scrape_page.page.title.blank?
+        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) belongs to a page with no title. Not caching."
+        return
+      end
       cache_atts = [{page_id: @scrape_page.page.id, text: @scrape_page.page.title, kind: 'title'}]
       insert_command = Cache::InsertQueriesAndResults.new(cache_atts)
       run_nested_with_gc!(insert_command)
@@ -28,6 +31,12 @@ module Cache
 
     def cache_links
       link_texts = @scrape_page.page.links_to.where.not(text: [nil, ""]).pluck(:text)
+
+      if link_texts.empty?
+        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) belongs to a page with no links_to. Not caching."
+        return
+      end
+
       cache_atts = link_texts.map do |link_text|
         {
             page_id: @scrape_page.page.id,
@@ -56,6 +65,11 @@ module Cache
       page_words = page_content.downcase.split(' ')
 
       query_ids = Query.where(text: page_words).pluck(:id)
+
+      if query_ids.empty?
+        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) has a page file that does not contain any words that match queries in our database.  Not caching."
+        return
+      end
 
       result_atts = query_ids.map {|q_id|  {query_id: q_id, page_id: @scrape_page.page.id, kind: 'body'} }
       Result.insert_all(result_atts, unique_by: :index_results_on_query_id_and_page_id_and_kind)
