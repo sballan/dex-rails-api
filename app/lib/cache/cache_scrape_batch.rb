b19a6eed2cd84181af9e7c5f0f3a84b488ce32fe
Samuel Ballan <sgb4622@gmail.com>
2020-12-08 13:10:02 -0500

    caching scrape batch uses CacheScrapePage

--- app/lib/cache/cache_scrape_batch.rb
+++ app/lib/cache/cache_scrape_batch.rb
@@ -6,16 +6,16 @@ module Cache
     end
 
     def run_proc
-      if scrape_batch.scrape_pages.parse_success.any? && (Time.now.to_i < end_time)
-        num_to_cache = scrape_batch.scrape_pages.parse_success.cache_ready.count
+      if @scrape_batch.scrape_pages.parse_success.any?
+        num_to_cache = @scrape_batch.scrape_pages.parse_success.cache_ready.count
 
-        Rails.logger.debug "[Cache::CacheScrapeBatch] More pages to cache! Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
-        Rails.logger.info "[Cache::CacheScrapeBatch] Starting loop of new Command::CreatePageQueries Command"
+        Rails.logger.debug "[Cache::CacheScrapeBatch] More pages to cache!"
+        Rails.logger.info "[Cache::CacheScrapeBatch] Starting loop of new Cache::CacheScrapePage Command"
 
-        scrape_batch.scrape_pages.parse_success.cache_ready.includes(:page).in_batches.each_record do |scrape_page|
+        @scrape_batch.scrape_pages.parse_success.cache_ready.includes(:page).in_batches.each_record do |scrape_page|
           scrape_page.cache_started_at = DateTime.now.utc
           scrape_page.cache_active!
-          command = Command::CreatePageQueries.new scrape_page.page
+          command = Cache::CacheScrapePage.new scrape_page
           command.run_with_gc!
 
           if command.success?
@@ -29,14 +29,11 @@ module Cache
           scrape_page.save!
         end
 
-        command = BatchUploadQueryResults.new
-        command.run_with_gc!
-
-        num_left = scrape_batch.scrape_pages.parse_success.cache_ready.count
+        num_left = @scrape_batch.scrape_pages.parse_success.cache_ready.count
         Rails.logger.debug "[Cache::CacheScrapeBatch] We went from #{num_to_cache} to #{num_left}."
 
       else
-        Rails.logger.info "[Cache::CacheScrapeBatch] No pages left to cache. Time left: #{end_time - Time.now.to_i}. RefreshStatus: #{scrape_batch.refresh_status}"
+        Rails.logger.info "[Cache::CacheScrapeBatch] No pages left to cache. "
       end
     end
 
