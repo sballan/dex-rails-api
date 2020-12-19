52ecea10bd9341f0cf9c045ddf39100769974b2e
Samuel Ballan <sgb4622@gmail.com>
2020-12-19 22:49:19 +0000

    refresh service beginning sketch

---
+++ app/lib/refresh_service.rb
@@ -0,0 +1,12 @@
+module RefreshService
+  extend self
+
+  def refresh_page(page, refresh_time=1.day.ago)
+  end
+
+  def refresh_pages(pages, refresh_time=1.day.ago)
+  end
+
+  def download_cached_page(page)
+  end
+end
