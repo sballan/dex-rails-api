d45f967e685cd0c3862f4141324a72cf8b54b191
Samuel Ballan <sgb4622@gmail.com>
2020-12-22 18:21:18 -0500

    does this loop work?

--- app/jobs/refresh_next_page_for_site_job.rb
+++ app/jobs/refresh_next_page_for_site_job.rb
@@ -2,20 +2,38 @@ class RefreshNextPageForSiteJob < ApplicationJob
   queue_as :refresh
 
   before_perform do |job|
+    site_id = job.arguments.first
     Site.transaction do
-      site_id = job.arguments.first
       site = Site.lock.find(site_id)
 
       if site.refresh_job_id.present? &&
         site.refresh_job_id != job_id &&
-        site.refresh_job_started_at < 10.minutes.ago
-        raise "Cannot run this job, another process is already running"
+        site.refresh_job_started_at > 10.minutes.ago
+        # Make sure this doesn't get retried, needs to be discarded
+        raise "Cannot run this job, it is already running on a different worker"
       end
 
       site.refresh_job_id = job_id
       site.refresh_job_started_at = DateTime.now.utc
       site.save!
     end
+
+    Rails.logger.info "Successfully locked job #{job_id} for Site(#{site_id})"
+  end
+
+  after_perform do |job|
+    site_id = job.arguments.first
+    Site.transaction do
+      site = Site.lock.find(site_id)
+
+      if site.refresh_job_id.present? && site.refresh_job_id == job_id
+        site.refresh_job_id = nil
+        site.refresh_job_started_at = nil
+        site.save!
+      end
+    end
+
+    Rails.logger.info "Successfully unlocked job #{job_id} for Site(#{site_id})"
   end
 
   def perform(site_id)
@@ -25,19 +43,22 @@ class RefreshNextPageForSiteJob < ApplicationJob
     Page.transaction do
       page = Page.lock.refresh_ready_by_site(site).first
       if page.nil?
-        Rails.logger.info "No pages to refresh"
+        RefreshNextPageForSiteJob.set(wait: 10.seconds).perform_later(site.id)
+        Rails.logger.info "No pages to refresh.  Try again in 1 minute."
         return
+      else
+        page.refresh_status = :active
+        page.refresh_started_at = DateTime.now.utc
+        page.save!
       end
-
-      page.refresh_status = :active
-      page.refresh_started_at = DateTime.now.utc
-      page.save!
     end
 
     RefreshService::Client.refresh_page(page)
 
     if Page.refresh_ready_by_site(site).any?
       RefreshNextPageForSiteJob.perform_later(site.id)
+    else
+      RefreshNextPageForSiteJob.set(wait: 1.hour).perform_later(site.id)
     end
   end
 end
