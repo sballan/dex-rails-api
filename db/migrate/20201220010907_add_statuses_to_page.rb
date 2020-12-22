d2227cdd88453d45eb7b4b109c13cae5417de605
Samuel Ballan <sgb4622@gmail.com>
2020-12-22 17:00:05 -0500

    oy vey

--- db/migrate/20201220010907_add_statuses_to_page.rb
+++ db/migrate/20201220010907_add_statuses_to_page.rb
@@ -4,10 +4,10 @@ class AddStatusesToPage < ActiveRecord::Migration[6.0]
     add_column :pages, :parse_status, :integer, default: 0
     add_column :pages, :index_status, :integer, default: 0
     add_column :pages, :cache_status, :integer, default: 0
-  end
 
-  add_index :pages, :refresh_status
-  add_index :pages, :parse_status
-  add_index :pages, :index_status
-  add_index :pages, :cache_status
+    add_index :pages, :refresh_status
+    add_index :pages, :parse_status
+    add_index :pages, :index_status
+    add_index :pages, :cache_status
+  end
 end
