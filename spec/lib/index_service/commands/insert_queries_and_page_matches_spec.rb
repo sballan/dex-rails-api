3c2faa5553cd50d803241645ed0e3e30359f31fa
Samuel Ballan <sgb4622@gmail.com>
2020-12-23 22:56:19 +0000

    second test

--- spec/lib/index_service/commands/insert_queries_and_page_matches_spec.rb
+++ spec/lib/index_service/commands/insert_queries_and_page_matches_spec.rb
@@ -36,8 +36,15 @@ describe IndexService::Commands::InsertQueriesAndPageMatches do
       @command.run!
     end
     
-    it 'can be created with the right attributes' do
-      expect(@command).to be
+    it 'creates a query' do
+      query = Query.find_by_text 'some_text'
+      expect(query).to be
+    end
+
+    it 'creates a PageMatch that belongs to that query' do
+      query = Query.find_by_text 'some_text'
+      page_match = PageMatch.last
+      expect(page_match.query).to eql(query)
     end
   end
 end
