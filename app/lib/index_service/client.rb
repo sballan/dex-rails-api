acc1da2d3cadc28cf8a77deafdda5f8b7f24fcfb
Samuel Ballan <sgb4622@gmail.com>
2020-12-24 05:41:34 -0500

    preparing the index client

--- app/lib/index_service/client.rb
+++ app/lib/index_service/client.rb
@@ -1,94 +1,70 @@
 module IndexService
   module Client
-    def initialize(scrape_page)
-      super()
-      @scrape_page = scrape_page
-    end
 
-    def run_proc
-      query_ids_set = Set.new
-
-      query_ids_set.merge(title_queries)
-      query_ids_set.merge(links_queries)
-      query_ids_set.merge(body_queries)
-
-      handle_success!
-      result.succeed!
+    def index_page(page, level)
+      command = Commands::IndexPage.new(page, level)
+      command.run_with_gc!
+    rescue StandardError => e
+      handle_index_failure(page)
+      raise e
     end
 
     private
 
-    def handle_success!
-      @scrape_page.index_status = :success
-      @scrape_page.index_finished_at = DateTime.now.utc
-      @scrape_page.save!
+    def handle_index_start(page)
+      page.index_status = :active
+      page.index_started_at = DateTime.now.utc
+      page.save!
 
-      Rails.logger.info "IndexScrapePage succeeded for ScrapePage #{@scrape_page.id}"
+      Rails.logger.info "Starting index for Page(#{page.id})"
     end
 
-    def title_queries
-      if @scrape_page.page.title.blank?
-        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) belongs to a page with no title. Not indexing."
-        return []
-      end
-      index_atts = [{page_id: @scrape_page.page.id, text: @scrape_page.page.title, kind: 'title'}]
-      insert_command = Index::InsertQueriesAndResults.new(index_atts)
-      run_nested_with_gc!(insert_command)
+    def handle_index_failure(page)
+      page.index_status = :failure
+      page.index_finished_at = DateTime.now.utc
+      page.save
 
-      insert_command.payload
+      Rails.logger.info "Index failed for Page(#{page.id})"
     end
 
-    def links_queries
-      link_texts = @scrape_page.page.links_to.where.not(text: [nil, ""]).pluck(:text)
+    def handle_index_success(page)
+      page.index_status = :success
+      page.index_finished_at = DateTime.now.utc
+      page.save!
 
-      if link_texts.empty?
-        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) belongs to a page with no links_to. Not indexing."
-        return []
-      end
-
-      index_atts = link_texts.map do |link_text|
-        {
-            page_id: @scrape_page.page.id,
-            text: link_text,
-            kind: 'link'
-        }
-      end
-      insert_command = Index::InsertQueriesAndResults.new(index_atts)
-      run_nested_with_gc!(insert_command)
-
-      insert_command.payload
+      Rails.logger.info "Index succeeded for Page(#{page.id})"
     end
 
-    def body_queries
-      fetch_command = Parse::FetchPageFile.new(@scrape_page.page.url)
-      run_nested_with_gc!(fetch_command)
-
-      doc = Nokogiri::HTML(fetch_command.payload)
-      doc.xpath('//script').remove
-      doc.xpath('//style').remove
-
-      page_content = Html2Text.convert doc.to_html.force_encoding('UTF-8')
-      page_words = page_content.downcase.split(' ')
-
-      query_ids = Query.where(text: page_words).pluck(:id)
-
-      if query_ids.empty?
-        Rails.logger.debug "ScrapePage (#{@scrape_page.id}) has a page file that does not contain any words that match queries in our database.  Not indexing."
-        return []
-      end
-
-      result_atts = query_ids.map do |q_id|
-        {
-          query_id: q_id,
-          page_id: @scrape_page.page.id,
-          kind: 'body',
-          created_at: DateTime.now.utc,
-          updated_at: DateTime.now.utc
-        }
-      end
-      Result.insert_all(result_atts, unique_by: :index_results_on_query_id_and_page_id_and_kind)
-
-      query_ids
-    end
+    # def body_queries
+    #   fetch_command = Parse::FetchPageFile.new(@scrape_page.page.url)
+    #   run_nested_with_gc!(fetch_command)
+    #
+    #   doc = Nokogiri::HTML(fetch_command.payload)
+    #   doc.xpath('//script').remove
+    #   doc.xpath('//style').remove
+    #
+    #   page_content = Html2Text.convert doc.to_html.force_encoding('UTF-8')
+    #   page_words = page_content.downcase.split(' ')
+    #
+    #   query_ids = Query.where(text: page_words).pluck(:id)
+    #
+    #   if query_ids.empty?
+    #     Rails.logger.debug "ScrapePage (#{@scrape_page.id}) has a page file that does not contain any words that match queries in our database.  Not indexing."
+    #     return []
+    #   end
+    #
+    #   result_atts = query_ids.map do |q_id|
+    #     {
+    #       query_id: q_id,
+    #       page_id: @scrape_page.page.id,
+    #       kind: 'body',
+    #       created_at: DateTime.now.utc,
+    #       updated_at: DateTime.now.utc
+    #     }
+    #   end
+    #   Result.insert_all(result_atts, unique_by: :index_results_on_query_id_and_page_id_and_kind)
+    #
+    #   query_ids
+    # end
   end
 end
