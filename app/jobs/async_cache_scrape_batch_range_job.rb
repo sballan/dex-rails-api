796a1ee991783a4ff3705c70683a74ac614ca5c5
Samuel Ballan <sgb4622@gmail.com>
2020-12-25 13:39:03 -0500

    remove dead jobs

--- app/jobs/async_cache_scrape_batch_range_job.rb
+++
@@ -1,9 +0,0 @@
-class AsyncCacheScrapeBatchRangeJob < ApplicationJob
-  queue_as :cache
-
-  def perform(scrape_batch_id, start, finish, of=25)
-    scrape_batch = ScrapeBatch.find scrape_batch_id
-    command = Cache::CacheScrapeBatchRange.new(scrape_batch, start, finish)
-    command.run_with_gc!
-  end
-end
