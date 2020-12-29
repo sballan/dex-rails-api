5b0c195c75a62fb633f7a58664763f71f0c84926
Samuel Ballan <sgb4622@gmail.com>
2020-12-29 16:27:33 -0500

    setting up the mixing to actually enqueue

--- app/lib/job_batch/mixin.rb
+++ app/lib/job_batch/mixin.rb
@@ -1,17 +1,25 @@
 # Meant to be mixed in to an instance of ActiveJob::Base
 module JobBatch::Mixin
+  PREFIX = "Job/"
+
   attr_reader :batch_id
 
   def self.included(mod)
     mod.before_enqueue do |job|
-      if Thread.current[JobBatch::THREAD_OPEN_BATCH_SYMBOL].present?
-        batch_id = Thread.current[JobBatch::THREAD_OPEN_BATCH_SYMBOL]
-        if JobBatch::Batch.exists?(batch_id)
-          batch = JobBatch::Batch.new(batch_id)
-          batch.add_job(job.job_id)
-        else
-          raise "This should not be possible, but Thread.current still has a batch id"
-        end
+      if Thread.current[JobBatch::THREAD_OPEN_BATCH_SYMBOL].blank?
+        raise "This should not be possible, but Thread.current has no batch_id"
+      end
+
+      batch_id = Thread.current[JobBatch::THREAD_OPEN_BATCH_SYMBOL]
+
+      unless JobBatch::Batch.exists?(batch_id)
+        raise "This should not be possible, but somehow we opened a Batch that does not exist"
+      end
+
+      batch = JobBatch::Batch.new(batch_id)
+      batch.with_lock do
+        JobBatch::Job.create(job.job_id, batch.id)
+        Rails.logger.debug "Successfully added Job #{job.job_id} to Batch #{batch.id}"
       end
     end
 
@@ -21,6 +29,14 @@ module JobBatch::Mixin
   end
 
   protected
+
+  def job_batch_job
+    @_job_batch_job ||= JobBatch::Job.new(job_id)
+  end
+
+  def batch
+
+  end
 
   def open_batch(&block)
     batch_id = 'find batch id'
