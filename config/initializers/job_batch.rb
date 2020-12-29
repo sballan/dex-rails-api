c2046dcbb039139c29a473eee5c1ae1d97246364
Samuel Ballan <sgb4622@gmail.com>
2020-12-29 16:16:08 -0500

    smarter prefixes

--- config/initializers/job_batch.rb
+++ config/initializers/job_batch.rb
@@ -7,6 +7,8 @@ module JobBatch
   PREFIX = "JobBatch/"
   LOCK_PREFIX = PREFIX + "Lock/"
   STORE_PREFIX = PREFIX + "Store/"
+  JOBS_PREFIX = PREFIX + "Jobs/"
+  BATCHES_PREFIX = PREFIX + "Batches/"
 
   THREAD_OPEN_BATCH_SYMBOL = :open_job_batch_id
 
