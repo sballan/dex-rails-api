85e7a9b09c7d87f26132ac0852d10ab4ef20d541
Samuel Ballan <sgb4622@gmail.com>
2020-12-30 12:05:58 -0500

    more lock tests

--- spec/lib/job_batch/lock_spec.rb
+++ spec/lib/job_batch/lock_spec.rb
@@ -6,15 +6,39 @@ describe JobBatch::Lock do
     allow(JobBatch).to receive(:redis).and_return(@mock_redis)
   end
 
-  context "private method" do
-    describe "write_lock" do
-      let(:test_lock_name) { 'test_lock_name' }
-      let(:redis_test_lock_name) { JobBatch::LOCK_PREFIX + test_lock_name }
+  let(:test_lock_name) { 'test_lock_name' }
+  let(:redis_test_lock_name) { JobBatch::LOCK_PREFIX + test_lock_name }
 
-      after(:each) do
-        @mock_redis.del(test_lock_name)
+  after(:each) do
+    @mock_redis.del(redis_test_lock_name)
+  end
+
+  describe "lock" do
+    context "lock is available" do
+      it "gets the lock" do
+        key = JobBatch::Lock.lock(test_lock_name)
+        found_key = @mock_redis.get(redis_test_lock_name)
+        expect(found_key).to eql(key)
       end
+    end
+  end
 
+  describe "unlock" do
+    it "can unlock existing lock" do
+      key = 'test_lock_key'
+      @mock_redis.set(redis_test_lock_name, key)
+      found_key = @mock_redis.get(redis_test_lock_name)
+      expect(found_key).to eql(key)
+
+      JobBatch::Lock.unlock(test_lock_name, key)
+
+      found_key = @mock_redis.get(redis_test_lock_name)
+      expect(found_key).to be_nil
+    end
+  end
+
+  context "private methods" do
+    describe "write_lock" do
       it "writes the correct key" do
         key = 'test_lock_key'
         JobBatch::Lock.send(:write_lock, test_lock_name, key)
@@ -24,14 +48,30 @@ describe JobBatch::Lock do
     end
 
     describe "fetch_lock_key" do
-      let(:test_lock_name) { 'test_lock_name' }
-      let(:redis_test_lock_name) { JobBatch::LOCK_PREFIX + test_lock_name }
-
       it "fetches the correct key" do
         key = 'test_lock_key'
         @mock_redis.set(redis_test_lock_name, key)
         found_key = JobBatch::Lock.send(:fetch_lock_key, test_lock_name)
         expect(key).to eql(found_key)
+      end
+    end
+
+    describe "delete_lock" do
+      it "deletes correct lock" do
+        other_test_lock_name = 'other_text_lock_name'
+        other_redis_test_lock_name = JobBatch::LOCK_PREFIX + other_test_lock_name
+        other_key = 'other_test_lock_key'
+        @mock_redis.set(other_redis_test_lock_name, other_key)
+
+        key = 'test_lock_key'
+        @mock_redis.set(redis_test_lock_name, key)
+        JobBatch::Lock.send(:delete_lock, test_lock_name)
+
+        found_key = @mock_redis.get(redis_test_lock_name)
+        other_found_key = @mock_redis.get(other_redis_test_lock_name)
+
+        expect(found_key).to be_nil
+        expect(other_found_key).to eql(other_key)
       end
     end
   end
