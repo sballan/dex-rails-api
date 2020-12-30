8edb7aaafca7e82b08116d314b513b77578efc0b
Samuel Ballan <sgb4622@gmail.com>
2020-12-30 03:39:11 +0000

    start job spec

---
+++ spec/lib/job_batch/job_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+describe JobBatch::Job do
+  before do
+    @mock_redis = MockRedis.new
+    allow(JobBatch).to receive(:redis).and_return(@mock_redis)
+  end
+
+  context "Basics" do
+    let(:job) { JobBatch::Job.create SecureRandom.uuid }
+
+    it "can be created with a job_id" do
+      expect(job).to be
+    end
+
+    it "has a Redis key" do
+      key = job.key
+      expect(@mock_redis.exists?(key)).to be_truthy 
+    end
+
+    it "has a Batch" do
+      expect(job.batch).to be  
+    end
+
+    it "can be found" do
+      j = JobBatch::Job.find(job.id)
+      expect(j.id).to eql(job.id)
+    end
+  end
+end
