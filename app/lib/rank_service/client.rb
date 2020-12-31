cfa300a9fd5a30affc0631e69c34e997f30c6600
Samuel Ballan <sgb4622@gmail.com>
2020-12-31 17:27:30 -0500

    rockin with the client

--- app/lib/rank_service/client.rb
+++ app/lib/rank_service/client.rb
@@ -1,5 +1,24 @@
 module RankService
   module Client
     extend self
+
+    def rank_from_start_page(start_page, max_size)
+      rank_pages = collect_pages(start_page, max_size)
+      calculate(rank_pages)
+
+    end
+
+    private
+
+    def collect_pages(start_page, max_size)
+      command = Commands::CollectPages.new(start_page, max_size)
+      command.run!
+      command.payload
+    end
+
+    def calculate(rank_pages)
+      command = Commands::Calculate.new(rank_pages)
+      command.run!
+    end
   end
 end
