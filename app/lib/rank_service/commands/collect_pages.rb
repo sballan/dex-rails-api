9bf380dacc040b97d4e024adf75a6b4e326bd822
Samuel Ballan <sgb4622@gmail.com>
2020-12-31 13:50:48 -0500

    speeder link finding

--- app/lib/rank_service/commands/collect_pages.rb
+++ app/lib/rank_service/commands/collect_pages.rb
@@ -17,15 +17,19 @@ module RankService::Commands
       while current_page.present? && pages.size < @max_pages
         break unless (pages.size + current_page.links_from.count) < @max_pages
 
-        current_page.links_from.each do |link|
+        current_page.links_from.includes(:from).each do |link|
           pages[link.from.id] ||= {
             page: link.from,
             links_added: false
           }
+          break unless pages.size < @max_pages
         end
 
+        Rails.logger.debug "Added page #{current_page.url}"
+
         pages[current_page.id][:links_added] = true
-        current_page = pages.find {|k, v| v[:links_added] == false}.last[:page]
+        current_page = pages.find {|k, v| v[:links_added] == false}
+        current_page = current_page.blank? ? nil : current_page.last[:page]
       end
 
       result.succeed!(pages.map {|k, v| v[:page] })
