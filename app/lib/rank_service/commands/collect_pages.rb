dcb65b01cc3f237829361c160b928a3971ddec31
Samuel Ballan <sgb4622@gmail.com>
2020-12-31 14:18:33 -0500

    wiring up backlinks

--- app/lib/rank_service/commands/collect_pages.rb
+++ app/lib/rank_service/commands/collect_pages.rb
@@ -7,32 +7,56 @@ module RankService::Commands
     end
 
     def run_proc
-      pages = {}
+      pages_map = {}
       current_page = @start_page
-      pages[current_page.id] = {
+      pages_map[current_page.id] = {
         page: current_page,
         links_added: false
       }
 
-      while current_page.present? && pages.size < @max_pages
-        break unless (pages.size + current_page.links_from.count) < @max_pages
+      while current_page.present? && pages_map.size < @max_pages
+        break unless (pages_map.size + current_page.links_from.count) < @max_pages
 
         current_page.links_from.includes(:from).each do |link|
-          pages[link.from.id] ||= {
+          pages_map[link.from.id] ||= {
             page: link.from,
             links_added: false
           }
-          break unless pages.size < @max_pages
+          break unless pages_map.size < @max_pages
         end
 
         Rails.logger.debug "Added page #{current_page.url}"
 
-        pages[current_page.id][:links_added] = true
-        current_page = pages.find {|k, v| v[:links_added] == false}
+        pages_map[current_page.id][:links_added] = true
+        current_page = pages_map.find {|_k, v| v[:links_added] == false}
         current_page = current_page.blank? ? nil : current_page.last[:page]
       end
 
-      result.succeed!(pages.map {|k, v| v[:page] })
+      rank_pages = convert_pages(pages_map)
+
+      result.succeed!(rank_pages)
+    end
+
+    private
+
+    def convert_pages(pages_map)
+      rank_pages = {}
+
+      pages_map.each_with_index do |o, i|
+        id = o.first
+        position = i + 1
+        start_rank = 1.0 / pages_map.size
+        rank_pages[id] ||= RankService::Page.new(id: id, position: position, start_rank: start_rank)
+      end
+
+      pages_map.each do |k, v|
+        next unless v[:links_added]
+        v[:page].links_from.pluck(:from_id).each do |from_id|
+          rank_pages[k].back_links << rank_pages[from_id]
+        end
+      end
+
+      rank_pages
     end
   end
 end
--- app/lib/rank_service/page.rb
+++ app/lib/rank_service/page.rb
@@ -1,7 +1,11 @@
 class RankService::Page
-  attr_reader :id, :position, :back_links, :start_rank
-  attr_accessor :finish_rank
-  def initialize(id:, position:, back_links:, start_rank:)
-    @id, @position, @back_links, @start_rank = id, position, back_links, start_rank
+  attr_reader :id, :position, :start_rank
+  attr_accessor :back_links, :finish_rank
+  def initialize(id:, position:, start_rank:)
+    @id = id
+    @position = position
+    @start_rank = start_rank
+    @back_links = []
+    @finish_rank = nil
   end
 end
