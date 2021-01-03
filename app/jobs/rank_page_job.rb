f33aa05c6ab22619a49118fe2c09421945de29eb
Samuel Ballan <sgb4622@gmail.com>
2021-01-03 14:06:07 -0500

    compact GC after rank

--- app/jobs/rank_page_job.rb
+++ app/jobs/rank_page_job.rb
@@ -6,5 +6,6 @@ class RankPageJob < ApplicationJob
   def perform(page_id, depth)
     page = Page.find(page_id)
     RankService::Client.rank_from_start_page(page, depth)
+    GC.compact
   end
 end
