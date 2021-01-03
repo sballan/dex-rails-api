6487ba5b06cce2a74f10ace40cde21f871b35713
Samuel Ballan <sgb4622@gmail.com>
2021-01-03 11:48:57 -0500

    cache_epoch simplifies things

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,13 +1,20 @@
 class Query < ApplicationRecord
+  CACHE_EPOCH = DateTime.new(0).utc
+
   has_many :page_matches
   has_many :pages, through: :page_matches
+
+  validates_presence_of :cached_at
 
   scope :text_like_any, ->(matches_array) {
     where(arel_table[:text].matches_any(matches_array))
   }
 
-  scope :next_to_cache, ->(limit=100) {
-    # Makes sure null cols are first, then ascending order
-    order('CASE WHEN cached_at IS NULL THEN 0 ELSE 1 END, cached_at').limit(limit)
+  scope :never_cached, ->() {
+    where(cached_at: CACHE_EPOCH)
+  }
+
+  scope :cached_before, ->(before_date) {
+    where(cached_at: CACHE_EPOCH.utc..before_date)
   }
 end
