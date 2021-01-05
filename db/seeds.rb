bcb0155a8c12f1e443df2d8086724e4569f1d282
Samuel Ballan <sgb4622@gmail.com>
2021-01-05 13:06:32 -0500

    runescape - why not?

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,10 @@ Page.create(url: "https://harrypotter.fandom.com/wiki/Main_Page", meta_attribute
 
 Site.create(home_url: "http://tolkiengateway.net/wiki/Tolkien_fandom", host: "tolkiengateway.net", scrape_active: true)
 Page.create(url: "http://tolkiengateway.net/wiki/Tolkien_fandom", meta_attributes: {fetch_status: :ready})
-
+Site.create(home_url: "https://oldschool.runescape.wiki", host: "oldschool.runescape.wiki", scrape_active: true)
+Page.create(url: "https://oldschool.runescape.wiki", meta_attributes: {fetch_status: :ready})
 
 Site.create(home_url: "https://en.wikipedia.org/wiki/Main_Page", host: "en.wikipedia.org", scrape_active: true)
 Page.create(url: "https://en.wikipedia.org/wiki/Main_Page", meta_attributes: {fetch_status: :ready})
+
+
