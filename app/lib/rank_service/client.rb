0eec3d01065d676313f7cf6158f74731dc231636
Samuel Ballan <sgb4622@gmail.com>
2021-01-04 22:34:45 -0500

    more GC

--- app/lib/rank_service/client.rb
+++ app/lib/rank_service/client.rb
@@ -3,27 +3,31 @@ module RankService
     extend self
 
     def rank_from_start_page(start_page, max_size)
+      GC.compact
+
       rank_pages = collect_pages(start_page, max_size)
       calculate(rank_pages)
       update_pages(rank_pages)
+
+      GC.compact
     end
 
     private
 
     def collect_pages(start_page, max_size)
       command = Commands::CollectPages.new(start_page, max_size)
-      command.run!
+      command.run_with_gc!
       command.payload
     end
 
     def calculate(rank_pages)
       command = Commands::Calculate.new(rank_pages)
-      command.run!
+      command.run_with_gc!
     end
 
     def update_pages(rank_pages)
       command = Commands::UpdatePages.new(rank_pages)
-      command.run!
+      command.run_with_gc!
     end
   end
 end
