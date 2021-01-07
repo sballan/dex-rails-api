dcac60df9317d19961dbbb3c9ff34371cef7abbf
Samuel Ballan <sgb4622@gmail.com>
2021-01-07 18:44:44 -0500

    typoe

--- db/migrate/20210107231947_add_index_fragments_to_page_meta.rb
+++ db/migrate/20210107231947_add_index_fragments_to_page_meta.rb
@@ -2,6 +2,6 @@ class AddIndexFragmentsToPageMeta < ActiveRecord::Migration[6.0]
   def change
     add_column :page_meta, :indexed_title, :boolean, default: false
     add_column :page_meta, :indexed_links, :boolean, default: false
-    add_column :page_meta, :indexed_headers_, :boolean, default: false
+    add_column :page_meta, :indexed_headers, :boolean, default: false
   end
 end
