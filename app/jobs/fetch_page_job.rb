d93b315f7f82b882b420dd141728de4e289b1d0e
Samuel Ballan <sgb4622@gmail.com>
2021-01-07 13:58:16 -0500

    simplify fetch

--- app/jobs/fetch_page_job.rb
+++ app/jobs/fetch_page_job.rb
@@ -3,42 +3,25 @@ class FetchPageJob < ApplicationJob
 
   queue_as :fetch
 
-  def perform(page_id, depth)
+  # If we can't get the lock, retry at random time between now and 5 minutes from now
+  rescue_from(ActiveLock::Errors::FailedToLockError) do
+    retry_job(wait: rand(5.minutes))
+  end
+
+  def perform(page_id)
     page = Page.includes(:meta).find(page_id)
 
     unless page.meta.present?
       Rails.logger.info "No metadata for Page(#{page_id}); inserting metadata record"
-      page.update(meta_attributes: {})
+      page.update(meta_attributes: { fetch_status: :ready })
     end
 
-    if page.meta.fetch_success? || page.meta.fetch_dead?
-      Rails.logger.info "Not fetching Page(#{page_id}), since fetch status is #{page.meta.fetch_status}"
+    # We can only fetch fetch pages if they are ready or failed
+    unless page.meta.fetch_ready? || !page.meta.fetch_failure?
+      Rails.logger.warn "Not fetching Page(#{page_id}), since fetch status is #{page.meta.fetch_status}"
       return
     end
 
     FetchService::Client.fetch(page)
-
-    return unless depth > 0
-
-    batch.open do
-      CrawlPageJob.perform_later(page.id, depth - 1)
-    end
-
-    # index_batch = JobBatch::Batch.create
-    batch.open do
-      IndexPageFragmentJob.perform_later(page_id, 'title')
-
-      if depth > 1
-        IndexPageFragmentJob.perform_later(page_id, 'links')
-      end
-
-      if depth > 2
-        IndexPageFragmentJob.perform_later(page_id, 'headers')
-      end
-    end
-
-    batch.open do
-      RankPageJob.perform_later(page.id, 150)
-    end
   end
 end
