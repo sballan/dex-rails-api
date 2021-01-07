439d3cd66c80daddb269c3272305b0f83fe4d50b
Samuel Ballan <sgb4622@gmail.com>
2021-01-07 18:50:55 -0500

    index callback draft

---
+++ app/jobs/index_page_callback_job.rb
@@ -0,0 +1,17 @@
+class IndexPageCallbackJob < ApplicationJob
+  include JobBatch::Mixin
+
+  queue_as :index
+
+  def perform(page_id, index_fields)
+    page_to_index = Page.includes(:meta).find(page_id)
+
+    success = index_fields[:title] == page_to_index.meta.indexed_title &&
+              index_fields[:links] == page_to_index.meta.indexed_links &&
+              index_fields[:headers] == page_to_index.meta.indexed_headers
+
+    page_to_index.meta.index_status = success ? :success : :failure
+    page_to_index.meta.index_finished_at = DateTime.now.utc
+    page_to_index.save!
+  end
+end
