683b952bca6ac3cdb69b0d8047620479a8b18719
Samuel Ballan <sgb4622@gmail.com>
2021-01-08 14:20:10 -0500

    rankpage job should have a default argument

--- app/jobs/rank_page_job.rb
+++ app/jobs/rank_page_job.rb
@@ -3,7 +3,7 @@ class RankPageJob < ApplicationJob
 
   queue_as :rank
 
-  def perform(page_id, depth)
+  def perform(page_id, depth=150)
     page = Page.find(page_id)
     RankService::Client.rank_from_start_page(page, depth)
   end
