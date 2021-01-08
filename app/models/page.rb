9f164b2d46ba2ab04a7cd41e300f64a5ca3e5cf2
Samuel Ballan <sgb4622@gmail.com>
2021-01-08 15:59:40 -0500

    depdendent destroy i guess...

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,17 +1,17 @@
 class Page < ApplicationRecord
   validates_presence_of :url
 
-  has_one :meta, class_name: "PageMeta"
+  has_one :meta, class_name: "PageMeta", dependent: :destroy
   accepts_nested_attributes_for :meta, update_only: true
 
   # This might seem a little backwards - but that's just because language is weird.
-  has_many :links_to, inverse_of: :from, foreign_key: :from_id, class_name: "Link"
-  has_many :links_from, inverse_of: :to, foreign_key: :to_id, class_name: "Link"
+  has_many :links_to, inverse_of: :from, foreign_key: :from_id, class_name: "Link", dependent: :destroy
+  has_many :links_from, inverse_of: :to, foreign_key: :to_id, class_name: "Link", dependent: :destroy
 
   has_many :pages_linked_to, through: :links_to, source: :to
   has_many :pages_linked_from, through: :links_from, source: :from
 
-  has_many :page_matches
+  has_many :page_matches, dependent: :destroy
   has_many :queries, through: :page_matches
 
   scope :by_site, ->(site) {
