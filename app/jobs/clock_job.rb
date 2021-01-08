ae5c1089ad9bdcdf5acc998b3a5cbf2b16e569b8
Samuel Ballan <sgb4622@gmail.com>
2021-01-07 19:36:01 -0500

    better logging

--- app/jobs/clock_job.rb
+++ app/jobs/clock_job.rb
@@ -18,6 +18,7 @@ class ClockJob < ApplicationJob
     page_to_crawl = Page.by_meta(crawl_status: :ready).first
     # Queue crawl job
     if page_to_crawl
+      Rails.logger.info "Queueing for crawl: #{page_to_crawl.id}"
       CrawlPageJob.perform_later(page_to_crawl.id)
     else
       Rails.logger.info "No pages are crawl_ready"
@@ -28,6 +29,7 @@ class ClockJob < ApplicationJob
     page_to_index = Page.by_meta(index_status: :ready).first
     # Queue index job
     if page_to_index
+      Rails.logger.info "Queueing for index: #{page_to_crawl.id}"
       IndexPageJob.perform_later(page_to_index.id)
     else
       Rails.logger.info "No pages are index_ready"
@@ -37,6 +39,7 @@ class ClockJob < ApplicationJob
     page_to_rank = Page.by_meta(rank_status: :ready).first
     # Queue rank job
     if page_to_rank
+      Rails.logger.info "Queueing for rank: #{page_to_crawl.id}"
       RankPageJob.perform_later(page_to_rank.id)
     else
       Rails.logger.info "No pages are rank_ready"
