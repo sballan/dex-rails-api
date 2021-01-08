d91a6891442b51d987ffe15f6da82be08a395a34
Samuel Ballan <sgb4622@gmail.com>
2021-01-08 12:58:08 -0500

    clean up the crawl tick

--- app/services/crawl_service/client.rb
+++ app/services/crawl_service/client.rb
@@ -1,27 +1,29 @@
 module CrawlService
+  MAX_CRAWL_PAGES = ENV.fetch("MAX_CRAWL_PAGES", 1)
+  MAX_CRAWL_TIME = ENV.fetch("MAX_CRAWL_TIME", 6.hours)
+
   module Client
     extend self
 
     def tick(&block)
-      # TODO: Remove this statement, which is useful in testing.
-      if Page.by_meta(crawl_status: :active).count > 1
-        Rails.logger.error "We can currently only crawl 1 page at a time"
-      end
+      # Update old PageMeta to have failed status. The assumption is that pages crawling longer than
+      # the MAX_CRAWL_TIME are actually not running.
+      # TODO: consider tracking thread ids in a way that would let us kill long running crawls etc.
+      PageMeta.where(crawl_status: :active, crawl_started_at: DateTime.new(0)..MAX_CRAWL_TIME.ago)
+          .update_all(crawl_status: :failure)
 
-      # Mark pages as failure if they've been crawling for more than 2 hours
-      page_crawling = Page.includes(:meta).by_meta(crawl_status: :active).first
-      if page_crawling && page_crawling.meta.crawl_started_at && (page_crawling.meta.crawl_started_at < 2.hour.ago)
-        Rails.logger.warn "Page(#{page_crawling.id}) has been crawling for more than 2 hours"
-        page_crawling.meta.update!(crawl_status: :failure)
-      end
+      # Now that we've marked failures, let's count real active pages
+      num_active_pages = Page.by_meta(crawl_status: :active).count
 
+      # Now let's collect pages to pass to the block
       page_ids = []
-
-
-      # Update all pages for active Sites that have crawl_status: :ready, which means they have already been fetched
-      # We limit to 5, since we can't ever possibly crawl 5 pages in less than 10 minutes...right? With other jobs
-      # happening too? Maybe we'll raise it one day.
-      page_ids.concat Page.by_meta(crawl_status: :ready).limit(5).pluck(:id)
+      if num_active_pages >= MAX_CRAWL_PAGES
+        Rails.logger.error "We're at crawl capacity with #{count} pages being crawled"
+      else
+        # Update all pages for active Sites that have crawl_status: :ready, which means they have already been fetched
+        num_additional_pages = MAX_CRAWL_PAGES - num_active_pages
+        page_ids.concat Page.by_meta(crawl_status: :ready).limit(num_additional_pages).pluck(:id)
+      end
 
       Rails.logger.info "Collected Page ids for tick"
       block.call(page_ids)
