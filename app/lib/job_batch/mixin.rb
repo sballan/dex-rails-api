0e9d61a9b392b20f6438b5f109cbdf5e0d81587d
Samuel Ballan <sgb4622@gmail.com>
2021-01-13 12:38:56 -0500

    hotfix for mixin

--- app/lib/job_batch/mixin.rb
+++ app/lib/job_batch/mixin.rb
@@ -6,15 +6,32 @@ module JobBatch::Mixin
 
   def self.included(mod)
     mod.before_enqueue do |job|
-      batch = JobBatch::Batch.opened_batch
+      jb_job = JobBatch::Job.find job.job_id
 
-      if JobBatch::Batch.opened_batch.blank?
-        Rails.logger.debug "There is no currently open batch, so let's create one"
-        batch = JobBatch::Batch.create
+      # If Job is present and is in batch, just queue it up.
+      if jb_job.present?
+        # If Job is present and is in batch, just queue it up.
+        if jb_job.batch.present?
+          Rails.logger.debug "Job already exists, enqueueing existing Job #{jb_job.id}"
+          return
+        else
+          # if Job is present but has not batch, it's an error!
+          raise "Job #{jb_job.id} already exists, but is not in Batch"
+        end
+      else
+        Rails.logger.info "Job does not exist, creating it now"
+
+        if JobBatch::Batch.opened_batch.present?
+          Rails.logger.debug "There is an open batch, so let's use it"
+          batch = JobBatch::Batch.opened_batch
+        else
+          Rails.logger.debug "There is no currently open batch, so let's create one"
+          batch = JobBatch::Batch.create
+        end
+
+        jb_job = JobBatch::Job.create(job.job_id, batch_id: batch.id)
+        Rails.logger.debug "Successfully added Job #{jb_job.id} to Batch #{batch.id}"
       end
-
-      jb_job = JobBatch::Job.find_or_create(job.job_id, batch_id: batch.id)
-      Rails.logger.debug "Successfully added Job #{jb_job.id} to Batch #{batch.id}"
     end
 
     mod.after_perform do |job|
