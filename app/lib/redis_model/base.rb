27109dad6f9ae804c53f4817e65a4598439c4a37
Samuel Ballan <sgb4622@gmail.com>
2021-01-16 09:17:27 -0500

    init redis_model base

---
+++ app/lib/redis_model/base.rb
@@ -0,0 +1,127 @@
+class RedisModel::Base
+  REDIS_PREFIX = "Base"
+  REDIS_HASH_KEYS = %w[id]
+  REDIS_DEFAULT_DATA = ->(id) { {id: id} }
+
+  include ActiveLock::Lockable
+  set_lock_id_name :id
+
+  attr_reader :id
+
+  def initialize(id)
+    # This will match an ActiveJob id
+    # TODO: does ActiveJob have a matcher for this?
+    @id = id.remove(/^#{self.class::REDIS_PREFIX}/).remove(/\/record$/)
+  end
+
+  def key
+    self.class.key_for(id)
+  end
+
+  def ==(other_object)
+    if other_object.respond_to? :key
+      key == other_object.key
+    else
+      false
+    end
+  end
+
+  def fetch_data
+    self.class.fetch_data(id)
+  end
+
+  def [](field_name)
+    value = self.class.redis.hmget(key, field_name)
+    value.first
+  end
+
+  def []=(field_name, field_value)
+    self.class.redis.hmset(key, field_name, field_value)
+  end
+
+  def self.each(&block)
+    Enumerator.new do |y|
+      redis.scan_each(match: self::REDIS_PREFIX + "*" + "/record") do |id|
+        y << new(id)
+      end
+    end
+  end
+
+  def self.find(id)
+    return nil unless exists?(id)
+
+    new(id)
+  end
+
+  def self.create(id=nil, attrs={})
+    raise "Invalid attrs" unless attrs.is_a?(Hash)
+
+    id ||= SecureRandom.uuid
+
+    raise "Cannot create #{self.name}: it already exists" if self.exists? id
+
+    attrs = self::REDIS_DEFAULT_DATA.call(id).merge(attrs)
+
+    res_multi = redis.multi do |multi|
+      multi.mapped_hmset(key_for(id), attrs)
+
+      # Make sure all belongs_to fields are set
+      belongs_to_klasses.each do |key, value|
+        next unless value[:required] == true
+
+        unless attrs.keys.include?(:"#{key}_id")
+          raise "#{self.name} cannot be created without a #{key} relation"
+        end
+      end
+
+      # Call <relation>_insert for each belongs_to relation
+      attrs.each do |key, value|
+        relation_name = key.to_s.remove(/_id$/).to_sym
+        next unless belongs_to_klasses.has_key?(relation_name)
+
+        # TODO: This doesn't work because we're doing a FIND in the middle of a MULTI? Connection pool messing this up?
+        # I don't even know how this _ever_ worked.  For now, comment out - and then consider just doing this check
+        # AFTER the multi is finished.
+        #
+        # relation = belongs_to_klasses[relation_name][:class].find(value)
+        # raise "Cannot find relation #{relation_name} #{value} for #{belongs_to_klasses[relation_name][:class]}" unless relation.present?
+        # relation.send(:"#{belongs_to_klasses[relation_name][:inverse_of]}_insert", id)
+        #
+        # TODO: refactor so we're not reaching into redis here
+        multi.sadd(relation_key_for(id, relation_name), id)
+      end
+    end
+
+    raise "Failed to create #{self.name}: Redis.multi failed" unless res_multi.first == "OK"
+    new(id)
+  end
+
+  def self.find_or_create(id, attrs)
+    with_lock(id) do
+      model = find(id)
+      return model unless model.nil?
+
+      return create(id, self::REDIS_DEFAULT_DATA.call(id).merge(attrs))
+    end
+  end
+
+  def self.fetch_data(id)
+    data = redis.mapped_hmget(key_for(id), *self::REDIS_HASH_KEYS).with_indifferent_access
+    raise unless data.present?
+
+    data
+  end
+
+  def self.key_for(id)
+    self::REDIS_PREFIX + id + "/record"
+  end
+
+
+  def self.exists?(id)
+    redis.exists?(key_for(id))
+  end
+
+  def self.redis
+    DEFAULT_REDIS
+  end
+end
