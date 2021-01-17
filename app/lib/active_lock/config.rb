fd240045508ce839a300b9b4220ed349876d1652
Samuel Ballan <sgb4622@gmail.com>
2021-01-16 23:06:52 -0500

    no need for such a short default lock ttl

--- app/lib/active_lock/config.rb
+++ app/lib/active_lock/config.rb
@@ -1,7 +1,7 @@
 module ActiveLock::Config
   extend self
 
-  DEFAULT_LOCK_TTL = 1.hour
+  DEFAULT_LOCK_TTL = 6.hours
   DEFAULT_LOCK_RETRY_TIME = 15.seconds
   DEFAULT_LOCK_RETRY_LENGTH = 0.01.seconds
 
