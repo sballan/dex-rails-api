8fe0b56dd0bdc57d04eba0b92f066d020ca812ca
Samuel Ballan <sgb4622@gmail.com>
2021-01-19 19:52:28 -0500

    uggg api changed

--- app/jobs/clock_job.rb
+++ app/jobs/clock_job.rb
@@ -11,7 +11,7 @@ class ClockJob < ApplicationJob
     start_time = Time.now
     Rails.logger.info "Clock Tick started"
 
-    ActiveLock::Lock.with_lock('GlobalClock', 10.minutes) do
+    ActiveLock::Lock.with_lock('GlobalClock', nil, ttl: 10.minutes) do
       JobBatch::Batch.all.each do |jb|
         begin
           jb.with_lock do |lock_key|
