bcf268ad5abf0b84188d723b7341377683d97541
Samuel Ballan <sgb4622@gmail.com>
2021-01-22 17:33:09 -0500

    react router

--- app/javascript/packs/dex.tsx
+++ app/javascript/packs/dex.tsx
@@ -1,21 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { HashRouter, Switch, Route } from "react-router-dom"
 
 import Home from '../dex/home'
+import Search from '../dex/search'
 
 class Dex extends React.Component<any, any>{
     render() {
         return (
-          <>
-            <Home/>
-          </>
+          <Switch>
+            <Route path="/search">
+              <Search />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
         )
     }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     ReactDOM.render(
-        <Dex />,
+        <HashRouter><Dex/></HashRouter>,
         document.body.appendChild(document.createElement('div')),
     )
 })
