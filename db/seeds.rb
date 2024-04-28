10889b12fdf0a117566d7be5acb3d182f126df68
Samuel Ballan <sgb4622@gmail.com>
2024-04-28 10:01:44 -0400

    add hackernews

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,12 @@ Page.create(url: "https://harrypotter.fandom.com/wiki/Main_Page", meta_attribute
 
 Site.create(home_url: "http://tolkiengateway.net/wiki/Tolkien_fandom", host: "tolkiengateway.net", scrape_active: true)
 Page.create(url: "http://tolkiengateway.net/wiki/Tolkien_fandom", meta_attributes: {fetch_status: :ready})
+
 Site.create(home_url: "https://oldschool.runescape.wiki", host: "oldschool.runescape.wiki", scrape_active: true)
 Page.create(url: "https://oldschool.runescape.wiki", meta_attributes: {fetch_status: :ready})
 
 Site.create(home_url: "https://en.wikipedia.org/wiki/Main_Page", host: "en.wikipedia.org", scrape_active: true)
 Page.create(url: "https://en.wikipedia.org/wiki/Main_Page", meta_attributes: {fetch_status: :ready})
+
+Site.create(home_url: "https://news.ycombinator.com", host: "news.ycombinator.com", scrape_active: true)
+Page.create(url: "https://news.ycombinator.com", meta_attributes: {fetch_status: :ready})
