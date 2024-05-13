93d778353a751fe5cf556dc3693fc5dedbef713c
Samuel Ballan <sgb4622@gmail.com>
2024-05-12 22:06:54 -0400

    add the old sitescraper

---
+++ app/jobs/old_site_scraper_job.rb
@@ -0,0 +1,11 @@
+class OldSiteScraperJob < ApplicationJob
+  queue_as :default
+
+  def perform(site_id = nil, depth = 4)
+    site_id ||= Site.first.id
+    site = Site.find(site_id)
+
+    scraper = SiteScraper.new(site)
+    scraper.scrape_to_depth(depth)
+  end
+end
