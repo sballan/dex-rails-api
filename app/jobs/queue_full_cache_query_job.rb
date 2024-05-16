dcffb8daa0e59dd7b7d627e7c29246e530629a46
Samuel Ballan <sgb4622@gmail.com>
2024-05-16 00:19:08 -0400

    async job to queue async jobs

---
+++ app/jobs/queue_full_cache_query_job.rb
@@ -0,0 +1,14 @@
+class QueueFullCacheQueryJob < ApplicationJob
+  queue_as :cache
+
+  def perform
+    counter = 0
+    Query.never_cached.in_batches.each_record do |q|
+      counter += 1
+      CacheQueryJob.perform_later(q.id)
+      if counter % 100_000 == 0
+        sleep 10.minutes
+      end
+    end
+  end
+end
