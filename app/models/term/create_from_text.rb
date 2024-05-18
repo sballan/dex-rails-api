385c3ca28b961f4d1991f4ae045212168f3a1861
Samuel Ballan <sgb4622@gmail.com>
2024-05-18 18:39:24 -0400

    term create from text

--- app/models/term/create_from_text.rb
+++ app/models/term/create_from_text.rb
@@ -1,11 +1,13 @@
 class Term::CreateFromText
-  attr_reader :text
+  attr_reader :text, :term_positions
+
   def initialize(text)
     @text = text
+    @term_positions = nil
   end
 
-  def call(text)
-    tokens = Term::Tokenizer.new(text).tokenize
+  def create_terms
+    tokens = token_positions.keys
 
     existing_terms = Term.where(term: tokens).index_by(&:term)
     new_terms = tokens - existing_terms.keys
@@ -13,6 +15,20 @@ class Term::CreateFromText
     return existing_terms if new_terms.empty?
 
     Term.insert_all(new_terms.map { |term| {term: term} })
-    existing_terms.values + Term.where(term: new_terms)
+    terms = existing_terms.values + Term.where(term: new_terms).to_a
+    @term_positions = terms.index_by(&:term)
+
+    terms
+  end
+
+  private
+
+  def token_positions
+    token_positions = {}
+    Term::Tokenizer.new(text).tokenize.each_with_index do |token, index|
+      token_positions[token] ||= []
+      token_positions[token] << index
+    end
+    token_positions
   end
 end
--- spec/models/term/create_from_text_spec.rb
+++ spec/models/term/create_from_text_spec.rb
@@ -3,40 +3,38 @@ require "rails_helper"
 RSpec.describe Term::CreateFromText, type: :model do
   describe "#call" do
     let(:text) { "Some text for testing" }
-    let(:service) { Term::CreateFromText.new(text) }
+    let(:term_create_from_text) { Term::CreateFromText.new(text) }
 
     context "when terms do not exist" do
       it "creates new terms" do
-        expect { service.call(text) }.to change { Term.count }.by(3)
+        expect { term_create_from_text.create_terms }.to change { Term.count }.by(3)
       end
 
       it "returns the created terms" do
-        terms = service.call(text)
+        terms = term_create_from_text.create_terms
         expect(terms.map(&:term)).to match_array(%w[some text test])
       end
     end
 
     context "when terms already exist" do
       before do
-        service.call(text)
+        term_create_from_text.create_terms
       end
 
       it "does not create new terms" do
-        expect { service.call(text) }.not_to change { Term.count }
+        expect { term_create_from_text.create_terms }.not_to change { Term.count }
       end
     end
 
     context "when some terms exist and some do not" do
-      let(:existing_term) { "Some" }
-      let(:new_term) { "newterm" }
-      let(:mixed_text) { "#{existing_term} #{new_term}" }
+      let(:existing_term) { "Some text" }
 
       before do
-        service.call existing_term
+        Term::CreateFromText.new(existing_term).create_terms
       end
 
       it "only creates new terms" do
-        expect { service.call(mixed_text) }.to change { Term.count }.by(1)
+        expect { term_create_from_text.create_terms }.to change { Term.count }.by(1)
       end
     end
   end
