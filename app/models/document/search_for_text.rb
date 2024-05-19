1cf6c5d0927c3f4f4cd61cf8fbce1d563adcb2c1
Samuel Ballan <sgb4622@gmail.com>
2024-05-19 11:33:25 -0400

    expand doc search space

--- app/models/document/search_for_text.rb
+++ app/models/document/search_for_text.rb
@@ -31,7 +31,7 @@ class Document::SearchForText
 
     query += " " + joins.join(" ") + " WHERE " + conditions.join(" AND ")
 
-    query += " \n" + "LIMIT 25"
+    query += " \n" + "LIMIT 1000"
 
     # Prepare the parameters
     proximity_params = Array.new(term_ids.size - 1, proximity)
